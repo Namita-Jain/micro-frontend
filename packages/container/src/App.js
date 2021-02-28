2e1044aceea0dfd2d71ac63113f125b26212d5a0
Namita Jain <namita.jain@joshtechnologygroup.com>
2021-02-28 20:20:53 +0530

    Randomized class name

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
+import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
+const genetateClassName = createGenerateClassName({
+    productionPrefix: 'co',
+})
 export default () => {
-    return <div>
-        <h1>Hello</h1>
-        <MarketingApp />
-    </div>
+    return (
+       <BrowserRouter>
+            <StylesProvider genetateClassName={genetateClassName}>
+                <div>
+                    <Header />
+                    <MarketingApp />
+                </div>
+            </StylesProvider>
+       </BrowserRouter>
+    )
 }
